04dee48bc643819df348e70c4fca37518ab54e03
Snir Sharristh <snirs@wix.com>
2022-09-04 12:42:22 +0300

    added wixlife profile search

--- src/utils/wixlife.rs
+++ src/utils/wixlife.rs
@@ -4,10 +4,20 @@ const FRAGMENT: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'<').add(b'>').ad
 
 
 pub fn construct_wix_life_search_url(query: &str) -> String {
-    let encoded_query = utf8_percent_encode(&query[3..], FRAGMENT).to_string();
-    let wix_life_search_url = format!(
-        "https://www.wix-life.com/search?q={}", encoded_query);
-    wix_life_search_url
+    if query == "wl" {
+        let wl_url = "https://www.wix-life.com";
+        wl_url.to_string()
+    } else if &query[..4] == "wl @" {
+        let encoded_query = utf8_percent_encode(&query[4..], FRAGMENT).to_string();
+        let wix_life_profile_url = format!(
+            "https://www.wix-life.com/profile/{}", encoded_query);
+        wix_life_profile_url
+    } else {
+        let encoded_query = utf8_percent_encode(&query[3..], FRAGMENT).to_string();
+        let wix_life_search_url = format!(
+            "https://www.wix-life.com/search?q={}", encoded_query);
+        wix_life_search_url
+    }
 }
 
 #[cfg(test)]
@@ -29,6 +39,15 @@ mod tests{
         assert_eq!(
             construct_wix_life_search_url(fake_query),
             "https://www.wix-life.com/search?q=hello%20world"
+        );
+    }
+
+    #[test]
+    fn test_construct_wl_profile_url() {
+        let fake_query = "wl @snirs";
+        assert_eq!(
+            construct_wix_life_search_url(fake_query),
+            "https://www.wix-life.com/profile/snirs"
         );
     }
 }
