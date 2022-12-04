4952a3e8c1646b724e3b849bdc6bada31df9b504
Snir Sharristh <snirs@wix.com>
2022-12-04 13:19:20 +0200

    fix yt

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn search(q: String) -> Redirect {
         "wl" => utils::wixlife::construct_wix_life_search_url(&q),
         "cal" => utils::calendar::construct_calendar_search_url(&q),
         "jr" => utils::jira::construct_jira_search_url(&q),
-        "yt" => utils::youtube::construct_youtube_search_url(&q),
+        "yt" => utils::youtube::construct_youtube_url(&q),
         _ => utils::google::construct_google_search_url(&q)
     };
     Redirect::to(redirect_url)
